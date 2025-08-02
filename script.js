7b24059bde52fc509e2395cbf13b745c1ec5f394
Pratik Chakraborty <pratik061203@gmail.com>
2025-08-03 04:08:43 +0530

    Fix asset loading and PWA bugs

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // --- FIREBASE IMPORTS ---
+// Forcing a change for Git to detect
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, signInAnonymously, signInWithCustomToken } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore, collection, addDoc, query, orderBy, limit, getDocs, setLogLevel } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
